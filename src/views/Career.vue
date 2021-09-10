e44f10792a7a04bb67b44affd8b8e5c39e6915cd
Nicolas Gabbani <nicolasgabbani@icloud.com>
2021-09-10 14:58:45 +0200

    update career

--- src/views/Career.vue
+++ src/views/Career.vue
@@ -32,13 +32,72 @@
       <div class="polygon blur"></div>
     </div>
     <h1>ma carrière</h1>
-    <div class="sub-title">J'ai travaillé pour CareerBuilder pendant 6 ans.</div>
+    <div class="sub-title">Et pourquoi pas devenir UX/UI Designer ?</div>
     <div class="main-society separator small-separator">
-      <h2>CareerBuilder</h2>
-      <div data-aos="zoom-in-up" data-aos-offset="100">
-        <div class="bg-img" :style="{ backgroundImage: `url(${require(`@/images/lesjeudis.jpg`)})` }"></div>
+      <h2>CtrlUp</h2>
+      <p class="main-society-desc">
+        Jeune ESN située à Bordeaux, j'ai eu la chance de faire parti de
+        l'équipe pendant 2 mois. Malheureusement, il a été très difficile de
+        trouver des missions et la décision de se séparer pour des raisons
+        financières a été prise.
+      </p>
+      <div data-aos="zoom-in-up" data-aos-offset="20">
+        <div
+          class="bg-img"
+          :style="{
+            backgroundImage: `url(${require(`@/images/iceapp.png`)})`,
+          }"
+        ></div>
         <div class="bg-white">
-          <h2><a href="https://www.lesjeudis.com/" target="_blank">LesJeudis.com</a></h2>
+          <h2>
+            <a href="https://www.iceapp.fr/" target="_blank">iceapp.fr</a>
+          </h2>
+          <div class="society">ICE APP via CtrlUp</div>
+          <p>
+            <span class="tag">React</span>
+            <span class="tag">NextJS</span>
+            <span class="tag">MangoDB</span>
+            <span class="tag">Responsive Design</span>
+          </p>
+          <p>
+            Ice est une application web proposant des quiz aux entreprises lors
+            de team building sur le thème de l'écologie.
+          </p>
+          <h4>Missions :</h4>
+          <ul>
+            <li>Développeur Front-End</li>
+            <li>Force de propositions pour améliorer l'application</li>
+            <li>Accompagnement du client</li>
+            <li>Mise en place des méthodes de travail (scrum)</li>
+          </ul>
+          <a href="https://www.iceapp.fr/" target="_blank" class="btn site-link"
+            >voir le site</a
+          >
+        </div>
+      </div>
+    </div>
+    <div class="main-society">
+      <h2>CareerBuilder</h2>
+      <p class="main-society-desc">
+        Société internationale proposant des sites d'emplois dans le monde
+        entier. J'ai participé à l'évolution de la société en étant un acteur
+        actif sur la création d'une plateforme web acceuillant pratiquement tous
+        les sites de la boite en tant que Développeur Front-End et UX/UI
+        Designer.
+      </p>
+      <div data-aos="zoom-in-up" data-aos-offset="100">
+        <div
+          class="bg-img"
+          :style="{
+            backgroundImage: `url(${require(`@/images/lesjeudis.jpg`)})`,
+          }"
+        ></div>
+        <div class="bg-white">
+          <h2>
+            <a href="https://www.lesjeudis.com/" target="_blank"
+              >LesJeudis.com</a
+            >
+          </h2>
           <div class="society">careerbuilder</div>
           <p>
             <span class="tag">RoR</span>
@@ -47,20 +106,40 @@
             <span class="tag">UX/UI</span>
             <span class="tag">From Scratch</span>
           </p>
-          <p>Site d'emploi spécialisé dans l'IT, mon rôle a été d'intégrer ce design à partir de maquettes sous le framework RubyOnRails. Tout le code front-end est from-scratch et fait maison !</p>
+          <p>
+            Site d'emploi spécialisé dans l'IT, mon rôle a été d'intégrer ce
+            design à partir de maquettes sous le framework RubyOnRails. Tout le
+            code front-end est from-scratch et fait maison !
+          </p>
           <h4>Missions :</h4>
           <ul>
             <li>Intégrateur/Développeur Front-End</li>
-            <li>Optimisation et simplification du code, maintenance des solutions</li>
+            <li>
+              Optimisation et simplification du code, maintenance des solutions
+            </li>
             <li>Designer pour la partie responsive du site</li>
           </ul>
-          <a href="https://www.lesjeudis.com/" target="_blank" class="btn site-link">voir le site</a>
+          <a
+            href="https://www.lesjeudis.com/"
+            target="_blank"
+            class="btn site-link"
+            >voir le site</a
+          >
         </div>
       </div>
       <div class="separator" data-aos="zoom-in-up" data-aos-offset="20">
-        <div class="bg-img" :style="{ backgroundImage: `url(${require(`@/images/jobmedic.jpg`)})` }"></div>
+        <div
+          class="bg-img"
+          :style="{
+            backgroundImage: `url(${require(`@/images/jobmedic.jpg`)})`,
+          }"
+        ></div>
         <div class="bg-white">
-          <h2><a href="https://www.jobmedic.co.uk/" target="_blank">jobmedic.co.uk</a></h2>
+          <h2>
+            <a href="https://www.jobmedic.co.uk/" target="_blank"
+              >jobmedic.co.uk</a
+            >
+          </h2>
           <div class="society">careerbuilder</div>
           <p>
             <span class="tag">RoR</span>
@@ -69,19 +148,44 @@
             <span class="tag">UX/UI</span>
             <span class="tag">From Scratch</span>
           </p>
-          <p>Job-board destiné aux métiers médicaux, j'ai pensé et designé (UI et UX) la première version du site entièrement en mobile first. <br> J'ai ensuite intégré le tout from scratch tout en apportant des améliorations quotidiennes. <br> Le design a peu à peu changé pour coller à celui du site principal de la société careerbuilder.com auquel j'ai aussi très activement participé.</p>
+          <p>
+            Job-board destiné aux métiers médicaux, j'ai pensé et designé (UI et
+            UX) la première version du site entièrement en mobile first. <br />
+            J'ai ensuite intégré le tout from scratch tout en apportant des
+            améliorations quotidiennes. <br />
+            Le design a peu à peu changé pour coller à celui du site principal
+            de la société careerbuilder.com auquel j'ai aussi très activement
+            participé.
+          </p>
           <h4>Missions :</h4>
           <ul>
             <li>Intégrateur et UI/UX designer</li>
-            <li>Optimisation et simplification du code, maintenance des solutions et expertise design (UI et UX)</li>
+            <li>
+              Optimisation et simplification du code, maintenance des solutions
+              et expertise design (UI et UX)
+            </li>
           </ul>
-          <a href="https://www.jobmedic.co.uk/" target="_blank" class="btn site-link">voir le site</a>
+          <a
+            href="https://www.jobmedic.co.uk/"
+            target="_blank"
+            class="btn site-link"
+            >voir le site</a
+          >
         </div>
       </div>
       <div class="separator" data-aos="zoom-in-up" data-aos-offset="20">
-        <div class="bg-img" :style="{ backgroundImage: `url(${require(`@/images/careerbuilder.png`)})` }"></div>
+        <div
+          class="bg-img"
+          :style="{
+            backgroundImage: `url(${require(`@/images/careerbuilder.png`)})`,
+          }"
+        ></div>
         <div class="bg-white">
-          <h2><a href="https://www.careerbuilder.com/" target="_blank">careerbuilder.com</a></h2>
+          <h2>
+            <a href="https://www.careerbuilder.com/" target="_blank"
+              >careerbuilder.com</a
+            >
+          </h2>
           <div class="society">careerbuilder</div>
           <p>
             <span class="tag">RoR</span>
@@ -90,56 +194,106 @@
             <span class="tag">UX/UI</span>
             <span class="tag">From Scratch</span>
           </p>
-          <p>Site principal de la société Careerbuilder, j'ai participé activement au design en me basant sur l'expérience acquise sur le projet jobmedic. Je l'ai ensuite intégré entièremment en continuant à être force de proposition tout le long du projet. <br> L'équipe étant internationale, la communication s'effectuait en anglais.</p>
+          <p>
+            Site principal de la société Careerbuilder, j'ai participé
+            activement au design en me basant sur l'expérience acquise sur le
+            projet jobmedic. Je l'ai ensuite intégré entièremment en continuant
+            à être force de proposition tout le long du projet. <br />
+            L'équipe étant internationale, la communication s'effectuait en
+            anglais.
+          </p>
           <h4>Missions :</h4>
           <ul>
             <li>Intégrateur et UI/UX designer</li>
-            <li>Optimisation et simplification du code, maintenance des solutions et expertise design (UI et UX)</li>
+            <li>
+              Optimisation et simplification du code, maintenance des solutions
+              et expertise design (UI et UX)
+            </li>
             <li>Présentation live hebdomadaire aux PO/Staff en anglais</li>
           </ul>
-          <a href="https://www.careerbuilder.com/" target="_blank" class="btn site-link">voir le site</a>
+          <a
+            href="https://www.careerbuilder.com/"
+            target="_blank"
+            class="btn site-link"
+            >voir le site</a
+          >
         </div>
       </div>
     </div>
     <div class="main-society">
       <h2>Autres projets</h2>
+      <p class="main-society-desc">
+        Mes projets personnels que j'ai principalement pratiqué lors de l'année
+        2020 pour m'auto formé aux framework JS (React et VueJS)
+      </p>
       <div data-aos="zoom-in-up" data-aos-offset="20">
-        <div class="bg-img" :style="{ backgroundImage: `url(${require(`@/images/superlicorne.jpg`)})` }"></div>
+        <div
+          class="bg-img"
+          :style="{
+            backgroundImage: `url(${require(`@/images/superlicorne.jpg`)})`,
+          }"
+        ></div>
         <div class="bg-white">
-          <h2><a href="http://superlicorne.com/" target="_blank">SuperLicorne.com</a></h2>
+          <h2>
+            <a href="http://superlicorne.com/" target="_blank"
+              >SuperLicorne.com</a
+            >
+          </h2>
           <div class="society">S.A.S Yoan Montalban</div>
           <p>
             <span class="tag">Vuejs</span>
             <span class="tag">animate.css</span>
             <span class="tag">nes.css</span>
           </p>
-          <p>Idle game où le but du jeu est d'avoir un maximum de fraises. Vous pouvez acheter des bâtiments, débloquer des succès, avoir des bonus aléatoires et acheter des améliorations. Le jeu a été développé from-scratch.</p>
+          <p>
+            Idle game où le but du jeu est d'avoir un maximum de fraises. Vous
+            pouvez acheter des bâtiments, débloquer des succès, avoir des bonus
+            aléatoires et acheter des améliorations. Le jeu a été développé
+            from-scratch.
+          </p>
           <h4>Missions :</h4>
           <ul>
             <li>Développeur Front-End</li>
             <li>Expertise UX</li>
             <li>Force de propositions pour améliorer le jeu</li>
           </ul>
-          <a href="http://superlicorne.com/" target="_blank" class="btn site-link">voir le site</a>
+          <a
+            href="http://superlicorne.com/"
+            target="_blank"
+            class="btn site-link"
+            >voir le site</a
+          >
         </div>
       </div>
       <div class="separator" data-aos="zoom-in-up" data-aos-offset="20">
-        <div class="bg-img" :style="{ backgroundImage: `url(${require(`@/images/pokemon.png`)})` }"></div>
+        <div
+          class="bg-img"
+          :style="{
+            backgroundImage: `url(${require(`@/images/sbbl.png`)})`,
+          }"
+        ></div>
         <div class="bg-white">
-          <h2><a href="https://pkm-api.herokuapp.com/" target="_blank">Pokemon</a></h2>
+          <h2>
+            <a href="#" target="_blank">Superblablaland.com</a>
+          </h2>
+          <div class="society">S.A.S Yoan Montalban</div>
           <p>
-            <span class="tag">Vuejs</span>
-            <span class="tag">aos</span>
-            <span class="tag">PokeAPI</span>
+            <span class="tag">ReactJS</span>
+            <span class="tag">GridJS</span>
+            <span class="tag">RoR</span>
           </p>
-          <p>À partir de l'API PokeAPI, découvrez les pokémons de première génération. Petit site sans prétention.</p>
+          <p>
+            Projet en cours de développement qui proposera un jeu multijoueur 2D
+            en ligne sur navigateur sous forme de tchat graphique avec une
+            boutique en ligne pour acheter principalement des skins.
+          </p>
           <h4>Missions :</h4>
           <ul>
             <li>Développeur Front-End</li>
             <li>Expertise UX/UI</li>
-            <li>Consommation d'API</li>
+            <li>Leader technique</li>
+            <li>Mise en place des méthodes de travail</li>
           </ul>
-          <a href="https://pkm-api.herokuapp.com/" target="_blank" class="btn site-link">voir le site</a>
         </div>
       </div>
     </div>
@@ -148,117 +302,125 @@
 </template>
 
 <script>
-import AOS from 'aos'
-import 'aos/dist/aos.css'
-import Rellax from 'rellax'
+import AOS from "aos"
+import "aos/dist/aos.css"
+import Rellax from "rellax"
 export default {
   mounted() {
     AOS.init()
-    new Rellax('.rellax')
-  }
+    new Rellax(".rellax")
+  },
 }
 </script>
 
 <style lang="sass" scoped>
-  .design-circle
-    background: var(--color-five)
-  .main-society
-    background-color: var(--color-white)
-    box-shadow: 0 0 25px 5px rgba(0,0,0,.4)
+.design-circle
+  background: var(--color-five)
+.main-society
+  background-color: var(--color-white)
+  box-shadow: 0 0 25px 5px rgba(0,0,0,.4)
 
-    padding: 20px
-    margin-bottom: 100px
+  width: 50vw
+  padding: 20px
+  margin-bottom: 100px
 
-    border-radius: 10px
-    
-    box-sizing: initial
-    h2
-      margin-bottom: 20px
-      text-align: center
-  .bg-img
-    height: 350px
-    width: 100%
-    border-radius: 10px
-    
-    background-repeat: no-repeat
-    background-size: cover
-    background-position: center center
-  .society
-    color: grey
-    text-transform: uppercase
-    font-size: .8rem
-    margin-bottom: 10px
-  .bg-white
-    background-color: var(--color-white)
-    box-shadow: 0 0 25px 5px rgba(0,0,0,.4)
+  border-radius: 10px
 
-    width: 30vw
-    margin: -300px auto 0 auto
-    padding: 30px
-    position: relative
-
-    border-radius: 10px
-    h4
-      margin-bottom: 10px
-    .tag
-      margin-right: 4px 
-      &:last-child
-        margin-right: 0
-  .site-link
-    display: block
-    margin-top: 20px
-
+  box-sizing: initial
+  h2
     text-align: center
-  .rellax1
-    position: absolute
-    bottom: 30%
-    left: 15%
-  .rellax2
-    position: absolute
-    bottom: 25%
-    left: 17%
-    .polygon
-      animation-delay: 1s
-  .rellax4
-    position: absolute
-    bottom: -30%
-    left: 10%
-  .rellax5
-    position: absolute
-    bottom: -30%
-    right: 14%
-    .polygon
-      animation-delay: 1s
-  .rellax6
-    position: absolute
-    top: 35%
-    right: 13%
-  .rellax7
-    position: absolute
-    bottom: 0
-    right: 18%
-  .rellax8
-    position: absolute
-    bottom: -70%
-    right: 13%
-    .polygon
-      animation-delay: 1s
-  .rellax9
-    position: absolute
-    bottom: -90%
-    right: 10%
-    .polygon
-      animation-delay: 1.5s
-  .rellax10
-    position: absolute
-    bottom: -80%
-    left: 10%
-    .polygon
-      animation-delay: .5s
-  .rellax11
-    position: absolute
-    bottom: -70%
-    left: 13%
-    .polygon
-      animation-delay: 1s
+
+  &-desc
+    text-align: center
+    font-style: italic
+    font-size: 0.8rem
+    width: 80%
+    margin: 0 auto
+    margin-bottom: 20px
+.bg-img
+  height: 350px
+  width: 100%
+  border-radius: 10px
+
+  background-repeat: no-repeat
+  background-size: cover
+  background-position: center center
+.society
+  color: grey
+  text-transform: uppercase
+  font-size: .8rem
+  margin-bottom: 10px
+.bg-white
+  background-color: var(--color-white)
+  box-shadow: 0 0 25px 5px rgba(0,0,0,.4)
+
+  width: 30vw
+  margin: -300px auto 0 auto
+  padding: 30px
+  position: relative
+
+  border-radius: 10px
+  h4
+    margin-bottom: 10px
+  .tag
+    margin-right: 4px
+    &:last-child
+      margin-right: 0
+.site-link
+  display: block
+  margin-top: 20px
+
+  text-align: center
+.rellax1
+  position: absolute
+  bottom: 30%
+  left: 15%
+.rellax2
+  position: absolute
+  bottom: 25%
+  left: 17%
+  .polygon
+    animation-delay: 1s
+.rellax4
+  position: absolute
+  bottom: -30%
+  left: 10%
+.rellax5
+  position: absolute
+  bottom: -30%
+  right: 14%
+  .polygon
+    animation-delay: 1s
+.rellax6
+  position: absolute
+  top: 35%
+  right: 13%
+.rellax7
+  position: absolute
+  bottom: 0
+  right: 18%
+.rellax8
+  position: absolute
+  bottom: -70%
+  right: 13%
+  .polygon
+    animation-delay: 1s
+.rellax9
+  position: absolute
+  bottom: -90%
+  right: 10%
+  .polygon
+    animation-delay: 1.5s
+.rellax10
+  position: absolute
+  bottom: -80%
+  left: 10%
+  .polygon
+    animation-delay: .5s
+.rellax11
+  position: absolute
+  bottom: -70%
+  left: 13%
+  .polygon
+    animation-delay: 1s
 </style>
